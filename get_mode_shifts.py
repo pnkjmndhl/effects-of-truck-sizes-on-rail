1b020715e72bffadd955213c7259d5a4f2a24a42
Pankaj Dahal <fusion.pankaj@gmail.com>
2020-03-24 15:18:21 -0400

    added speed, added model2
    added variable speed limits for each modes, added working model 2

--- get_mode_shifts.py
+++ get_mode_shifts.py
@@ -6,25 +6,28 @@ from scipy.optimize import curve_fit
 
 
 #constants
-data_df = pd.read_csv("pred2.csv").loc[:, 'dist_bin':'percent']
+truck_speed = [60,60,60,60] #mph
+rail_speed = 40 #mph
+base = 0
+compare = 1
+
+
+data_df = pd.read_csv("pred2.csv").loc[:, 'dist_bin':'use_rate_bin']
 rail_rate_df = pd.read_csv("input/RATES.csv").loc[:, 'SCTG':'RTM']
 truck_rate_df = pd.ExcelFile("input/Rate Table per ton.xlsx").parse("Sheet1")
 
 
-def func(x,a,b):
-    return (a*x*x+b*x+c)
 
 #select the columns from start to end
 model1_df = pd.ExcelFile('./input/Modeparms(compare).xlsx').parse("Shpmt Freight Rate Models").loc[0:35, 'SCTG':'Group']
-model2_df = pd.ExcelFile('./input/Modeparms(compare).xlsx').parse("23-Mkt Share GC").loc[1:23, 'SCTG':'F']
-#model3_df =
+model2_df = pd.ExcelFile('./input/Modeparms(compare).xlsx').parse("22-Mkt Share Frt-Trans time").loc[0:3, 'SCTG':'Group']
 
 
-(commodty,ann_tonnage,dist_bin) = ('"13"', 173.825835, 50)
+#args = (50, '"21"', 2000)
 
-get_share(commodty, ann_tonnage, dist_bin)
-def get_share(commodty, ann_tonnage, dist_bin):
-    #print ("{0}, {1}, {2}".format(commodty, ann_tonnage, dist_bin))
+#get_share(args)
+def get_share(args):
+    commodty, ann_tonnage, dist_bin = args[1], args[2], args[0]
     try:
         rl_rate_p_tm = float(rail_rate_df[(rail_rate_df['SCTG'].astype(str) == commodty) & (rail_rate_df['DGROUP'] == dist_bin)]['RTM'])
     except:
@@ -33,8 +36,9 @@ def get_share(commodty, ann_tonnage, dist_bin):
         rl_rate_p_tm1 = rl_rate_p_tm1.sort_values(by='DGROUP').reset_index()[['DGROUP', 'RTM']]
         rl_rate_p_tm1 = rl_rate_p_tm1.interpolate(method='polynomial', axis=0, order = 3).ffill().bfill()
         rl_rate_p_tm = float(rl_rate_p_tm1[rl_rate_p_tm1['DGROUP']==dist_bin]['RTM'])
-    rate_pton_const = truck_rate_df[dist_bin].loc[0:1].tolist() #just first and second row
-    cost = [rl_rate_p_tm*ann_tonnage*dist_bin, rate_pton_const[0]*ann_tonnage, rate_pton_const[1]*ann_tonnage]
+    truck_rate_pton_const = truck_rate_df[dist_bin].tolist()
+    truck_rate_pton_const = [y for (x,y) in enumerate(truck_rate_pton_const) if x in [base,compare]]
+    cost = [rl_rate_p_tm*ann_tonnage*dist_bin, truck_rate_pton_const[0]*ann_tonnage, truck_rate_pton_const[1]*ann_tonnage]
     #calculation
     try: #model1
         b0 = float(model1_df[model1_df.SCTG == int(commodty.replace('"',""))]['b0'])
@@ -45,19 +49,25 @@ def get_share(commodty, ann_tonnage, dist_bin):
         Ut = [(gen_b0 + b0) + x*(br+ gen_br) for x in cost[1:]]
     except:
         try: #model2
-            b0 = float(model2_df[model2_df.SCTG == int(commodty.replace('"', ""))]['b0'])
-            br = float(model2_df[model2_df.SCTG == int(commodty.replace('"', ""))]['bGC5'])
-            gen_b0 = model2_df.loc[0:0, :]['b0'][0]
-            gen_br = model2_df.loc[0:0, :]['br'][0]
-            Ur = cost[0] * (gen_b0 + b0)
-            Ut = [(gen_b0 + b0) + x * (br + gen_br) for x in cost[1:]]
+            b0 = float(model2_df[model2_df.SCTG == int(commodty.replace('"',""))]['b0'])
+            bC = float(model2_df[model2_df.SCTG == int(commodty.replace('"',""))]['bC'])
+            bT = float(model2_df[model2_df.SCTG == int(commodty.replace('"',""))]['bT'])
+            Ur = bC * cost[0] + bT *  float(dist_bin) / rail_speed  #distance in miles divided by speed in mph
+            _truck_speed_ = [y for (x,y) in enumerate(truck_speed) if x in [base,compare]]
+            cost_transit_time = zip(cost[1:], [float(dist_bin)/x for x in _truck_speed_ ])
+            Ut = [b0 + bC * x + bT *  y for x,y in cost_transit_time]  #distance in miles divided by speed in mph
         except:
-            return [-99,-99]
+            print ("{0}, {1}, {2}".format(commodty, ann_tonnage, dist_bin))
+            return -99,-99,-99, -99, -99
     #print cost
     #print ("{0}.{1}".format(Ur, Ut))
     pt = [1.0/(1+np.exp(Ur-x)) for x in Ut]
-    return pt[1]-pt[0]
+    return Ur, Ut[0], Ut[1], pt[0], pt[1]
 
 
-data_df['new'] = data_df.apply(lambda x: get_share(x['commodty'], x['sum_wt'], x['dist_bin']), axis = 1)
+
+#data_df1[['Ur','Ut0','Ut1','pt0','pt1']] = []
+#data_df1[['Ur','Ut0','Ut1','pt0','pt1']] = zip(*data_df.apply(get_share, axis = 1))
+data_df['Ur'], data_df['Ut0'], data_df['Ut1'], data_df['pt0'], data_df['pt1'] = zip(*data_df.apply(get_share, axis = 1))
+
 data_df.to_csv("mode_split.csv")
